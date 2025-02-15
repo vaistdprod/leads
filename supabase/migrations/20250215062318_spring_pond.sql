bdc475f29dae76b1a71e0bcaabc034b542b45fa5
Matěj Vais <vais@tdprod.cz>
2025-02-15 07:32:22 +0100

    v3.0.37

--- supabase/migrations/20250215062318_spring_pond.sql
+++ supabase/migrations/20250215062318_spring_pond.sql
@@ -2,4 +2,4 @@
 ALTER TABLE settings ADD COLUMN IF NOT EXISTS impersonated_email text;
 
 -- Add comment explaining the column
-COMMENT ON COLUMN settings.impersonated_email IS 'Email address to impersonate when sending emails';20250215062318_spring_pond.sql
+COMMENT ON COLUMN settings.impersonated_email IS 'Email address to impersonate when sending emails';
